199dd20480f4e1355dd7dc5c14c9e914b3e45685
RyoichiNakai <elmobasketball0913@me.com>
2021-09-05 17:31:50 +0900

    #19 add component

---
+++ components/cards/CardTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ma-3">
+    <v-card-title>
+      <h4>{{ title }}</h4>
+    </v-card-title>
+    <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+
+    <v-divider class="mx-auto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardTitle',
+  props: {
+    title: {
+      type: String,
+      default: 'デフォルトタイトル',
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: 'デフォルトサブタイトル',
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
