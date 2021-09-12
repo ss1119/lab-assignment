9f3596a29cb0c076904607cf362f162e0837ec02
RyoichiNakai <elmobasketball0913@me.com>
2021-09-12 14:20:28 +0900

    #47 base

--- components/admin/dialogs/FileExport.vue
+++ components/admin/dialogs/FileExport.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>FileExport</div>
+  <v-card>
+    <v-container class="form__scroll"> ああああ </v-container>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'FileExport',
+  name: 'Email',
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  mounted() {
+    this.isOpen = this.open
+  },
+  methods: {
+    globalEscape() {
+      this.isOpen = false
+      this.$emit('close', this.isOpen)
+    },
+    close() {
+      this.globalEscape()
+    },
+    save() {
+      this.globalEscape()
+    },
+  },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.form {
+  &__scroll {
+    max-height: 500px;
+  }
+  &__wrap {
+    width: 90%;
+    margin: 0 auto;
+  }
+}
+</style>
