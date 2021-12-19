1ebc39e6bb91a75a6595350b84d198db201c241d
RyoichiNakai <elmobasketball0913@me.com>
2021-12-19 22:55:09 +0900

    #72 管理者ログイン機能 追加 ブラウザセッション 調整

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,19 +1,24 @@
 export default ({ store, redirect, route }) => {
   const isLoggined = store.getters['auth/isLoggined']
   const isAdmin = store.getters['auth/isAdmin']
-  const logoutMenu = store.state.menu.logoutMenu
+  const loginMenu = store.state.menu.loginMenu
   const adminMenu = store.state.menu.adminMenu
-  const logoutUrl = logoutMenu.map((item) => item.url)
+  const loginUrl = loginMenu.map((item) => item.url)
   const adminUrl = adminMenu.map((item) => item.url)
 
   // ログアウト状態かつログインしている状態でしか見れないURLの場合
-  if (!isLoggined && !isAdmin && !logoutUrl.includes(route.path)) {
+  if (!isLoggined && !isAdmin && (loginUrl.includes(route.path) || adminUrl.includes(route.path))) {
     // ログイン画面に遷移
     redirect('/')
   }
   // ログイン状態の場合
-  if ((isLoggined || isAdmin) && route.path === '/') {
+  if (isLoggined && !isAdmin && route.path === '/') {
     redirect('/user')
+  }
+
+  // 管理者ログイン状態の場合
+  if (!isLoggined && isAdmin && route.path === '/') {
+    redirect(process.env.ADMIN_ROOT_URL)
   }
 
   // ログイン状態かつ管理者画面に遷移しようとした場合
@@ -22,7 +27,7 @@ export default ({ store, redirect, route }) => {
   }
 
   // 管理者ログインの場合
-  if (!isLoggined && isAdmin && !adminUrl.includes(route.path)) {
+  if (!isLoggined && isAdmin && loginUrl.includes(route.path)) {
     // 管理者画面にしか遷移できない
     redirect(process.env.ADMIN_ROOT_URL)
   }
