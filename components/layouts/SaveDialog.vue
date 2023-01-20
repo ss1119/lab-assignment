b4555369fa7590b8fdd2bd4b003434d5ad615560
ss1119 <y.sasaki111919@gmail.com>
2023-01-20 14:15:32 +0900

    保存確定ダイアログ作成

---
+++ components/layouts/SaveDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-dialog :value="open" width="450px" @input="close">
+    <v-card>
+      <v-card-title>
+        {{ cardTitle }}
+      </v-card-title>
+
+      <v-card-text>
+        <p>{{ cardText }}</p>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="close"> 閉じる </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'SaveDialog',
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      cardTitle: '得点を保存しました',
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('close', false)
+    },
+  },
+}
+</script>
+
+<style></style>
--- components/layouts/index.js
+++ components/layouts/index.js
@@ -1,5 +1,6 @@
 import Header from './Header'
 import NavDrawer from './NavDrawer'
 import LogOutDialog from './LogOutDialog'
+import SaveDialog from './SaveDialog'
 
-export { Header, NavDrawer, LogOutDialog }
+export { Header, NavDrawer, LogOutDialog, SaveDialog }
